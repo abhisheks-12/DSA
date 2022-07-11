f4af75ff5b6b511a64683c5d61e820d32e5a740e
Abhishek Sankpal <abhishek.sankpal12@gmail.com>
2022-07-11 16:03:52 +0530

    Hello World

--- Java Basics/Hello/StringLiterals_02.java
+++ Java Basics/Hello/StringLiterals_02.java
@@ -2,15 +2,15 @@
 public class StringLiterals_02 {
     public static void main(String[]args){
         //        String Literals
-        byte age = 34;
-        char firstCharacterOfName  = 'A';
-        float ageFloat = 10.5F;
-        double ageDouble = 10.11D;
-        int myAge = 23;
-        long dinosaurAge = 566666688888L;
-        short age3 = 15;
-        boolean myName = false;
-        String firstName  = "Abhishek";
+        byte age = 34;                                          // takes 1 byte( 8 bits)
+        short age3 = 15;                                     // takes 2 bytes (16 bits)
+        int myAge = 23;                                      // takes 4 bytes (32 bits)
+        long dinosaurAge = 566666688888L;                      // 8 bytes 
+        char firstCharacterOfName  = 'A';                       // 2 bytes
+        float ageFloat = 10.5F;                                 // 4 bytes
+        double ageDouble = 10.11D;                              // 8 bytes
+        boolean myName = false;                                 // depend on jvm
+        String firstName  = "Abhishek";                          
 
         System.out.println(myName);
 
