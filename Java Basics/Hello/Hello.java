d3ead78884cbae4b0c297a00366382d0904bfd0c
Abhishek Sankpal <abhishek.sankpal12@gmail.com>
2022-07-11 11:36:58 +0530

    hello world

---
+++ Java Basics/Hello/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        int number1 = 10;
+        int number2 = 15;
+        int number3 = 5;
+        int sum = number1 + number2 + number3;
+        System.out.println("Sum of numbers is : "+ sum);
+    }
+}
---
+++ Java Basics/Hello/StringLiterals_02.java
@@ -0,0 +1,19 @@
+
+public class StringLiterals_02 {
+    public static void main(String[]args){
+        //        String Literals
+        byte age = 34;
+        char firstCharacterOfName  = 'A';
+        float ageFloat = 10.5F;
+        double ageDouble = 10.11D;
+        int myAge = 23;
+        long dinosaurAge = 566666688888L;
+        short age3 = 15;
+        boolean myName = false;
+        String firstName  = "Abhishek";
+
+        System.out.println(myName);
+
+
+    }
+}
